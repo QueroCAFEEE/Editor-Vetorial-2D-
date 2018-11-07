31960c57b7430d97d819d66b6f2836f8d609fa12
Rafael Padilha <rafaelpadilha@users.noreply.github.com>
2018-11-07 10:28:40 -0200

    Documentação

--- ListaDesenhos.h
+++ ListaDesenhos.h
@@ -1,37 +1,42 @@
+//Estrutura para representar de maneira simplificada um ponto, armazenando as coordenadas do mesmo.
 typedef struct pt
 {
     int x;
     int y;
 } point;
 
+//Estrutura para simplificar a definição de cores.
 typedef struct color{
     double r;
     double g;
     double b;
-    double t;
+    double t;//alpha
 }Cor;
 
+//Estrutura para representar os vértices do polígono.
 typedef struct vert{
     //Info
     int x;
     int y;
 
     //Ponteiros
-    struct vert *prox;
-    struct vert *ant;
+    struct vert *prox;  //Próximo vértice, caso exita.
+    struct vert *ant;   //Vértice anterior, caso exista.
 }Vertice;
 
+//Estrutura para armazenar as informações dos poligonos na memória.
 typedef struct des{
     //Info
-    int ordemDesenho;
-    Cor corPolig;
-    int qtdVertices;
+    int ordemDesenho;   //Ordem que o polígono foi desenhado.
+    Cor corPolig;       //Cor do polígono
+    int qtdVertices;    //Quantidade de vértices que o polígono possui.
     //Ponteiros
-    Vertice *vertices;
-    struct des *prox;
-    struct des *ant;
+    Vertice *vertices;  //Primeiro endereço da lista de vértices caso exista.
+    struct des *prox;   //Próximo vértice, caso exista.
+    struct des *ant;    //Vértice anterior, caso exista.
 }Desenho;
 
+//Lista de desenhos, para armazenar o primeiro endereço da lista, ou null caso não exista desenhos na lista.
  typedef struct listaDesenhos{
     Desenho* head;
  }ListaD;
@@ -78,89 +83,89 @@ void imprime_info_desenhos(ListaD* lista);
 
 /*
  *  <Função>:               adiciona_vertice_desenho
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Parametros>:           Estrutura Desenho a qual o vértice pertence, Estutura vértice a ser adicionado ao desenho.
+ *  <Funcionalidade>:       Adiciona o vértice a lista de vértices do desenho.
  *  <Retorno>:              Não retorna nada.
  */
 void adiciona_vertice_desenho(Desenho* pol,Vertice* ve);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               adiciona_novo_desenho
+ *  <Parametros>:           Estrutura ListaD onde o desenho vai ser adicionado, Estrutura Desenho a ser adicionado à lista de polígonos.
+ *  <Funcionalidade>:       Adiciona o desenho à lista de polígonos informada.
  *  <Retorno>:              Não retorna nada.
  */
 void adiciona_novo_desenho(ListaD* lista, Desenho* novo);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
- *  <Retorno>:              Não retorna nada.
+ *  <Função>:               clica_desenho
+ *  <Parametros>:           Lista de poligonos, coordenadas x e y do clique do mouse.
+ *  <Funcionalidade>:       Varre a lista de polígonos verificando se o ponto onde ocorreu o clique se encontra dentro do intervalo dos vértices do polígono.
+ *  <Retorno>:              Caso exista um poligono, retorta o endereço desse poligono. Caso exista mais de um poligono, ele retorna o mais à cima (com maior odem).
  */
 Desenho* clica_desenho(ListaD* lista, int x, int y);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               move_desenho
+ *  <Parametros>:           Desenho a ser movido, variação x e y.
+ *  <Funcionalidade>:       Soma todas as coordenadas X com a varx e as coordenadas Y com vary do vértice, movendo assim o polígono.
  *  <Retorno>:              Não retorna nada.
  */
 void move_desenho(Desenho* pol, int varx,int vary);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               apaga_desenho
+ *  <Parametros>:           Lista de desenhos, desenho a ser apagado
+ *  <Funcionalidade>:       Varre a lista de desenhos procurando o desenho selecionado, caso ache o algoritmo aplica a lógica de remover da lista duplamente encadeada e libera a memória do desenho.
  *  <Retorno>:              Não retorna nada.
  */
 void apaga_desenho(ListaD* lista,Desenho* pol);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               escala_desenho
+ *  <Parametros>:           Desenho a ser escalado, nova escala do poligono.
+ *  <Funcionalidade>:       Escala os vértices do polígono em relação ao ponto médio do "envelope".
  *  <Retorno>:              Não retorna nada.
  */
 void escala_desenho(Desenho* pol, double escala);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               espelhamento
+ *  <Parametros>:           Desenho a ser espelhado.
+ *  <Funcionalidade>:       Espelha os vértices do polígono em relação a o eixo Y.
  *  <Retorno>:              Não retorna nada.
  */
 void espelhamento(Desenho* pol);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
+ *  <Função>:               salva_arquivo
+ *  <Parametros>:           Nome do arquivo, lista de desenhos à ser salva.
+ *  <Funcionalidade>:       Salva as informações contidas na estrutura em um arquivo para que possa ser carregada mais tarde.
  *  <Retorno>:              Não retorna nada.
  */
 int salva_arquivo(char* nome,ListaD* desenhos);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
- *  <Retorno>:              Não retorna nada.
+ *  <Função>:               carrega_arquivo
+ *  <Parametros>:           Nome do arquivo.
+ *  <Funcionalidade>:       Carrega as informações salvas no arquivo para a estrutura do programa.
+ *  <Retorno>:              Retorna a lista de desenhos gerada pelo algoritmo.
  */
 ListaD* carrega_arquivo(char* nome);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
- *  <Retorno>:              Não retorna nada.
+ *  <Função>:               distancia_entre_2pt
+ *  <Parametros>:           Coordenadas x e y de dois pontos.
+ *  <Funcionalidade>:       Função genérica para calcular a distãncia entre dois pontos.
+ *  <Retorno>:              Retorna a distãncia entre os dois pontos.
  */
 double distancia_entre_2pt(int x1,int x2,int y1,int y2);
 
 /*
- *  <Função>:
- *  <Parametros>:
- *  <Funcionalidade>:
- *  <Retorno>:              Não retorna nada.
+ *  <Função>:               ponto_medio
+ *  <Parametros>:           Desenho.
+ *  <Funcionalidade>:       Utiliza a idéia do envelope e calcula o centro do retângulo, achando assim aproximadamente o ponto médio do polígono.
+ *  <Retorno>:              Estrutura point com as coordenadas do ponto médio.
  */
 point ponto_medio(Desenho* poligono);
 
